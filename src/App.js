f4c5853c1c6005aa9555f7f0185d647a7a8f2d8f
Red-Fool48 <AniBro@users.noreply.github.com>
2020-12-14 22:41:53 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/homepage/homepage.component';
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;

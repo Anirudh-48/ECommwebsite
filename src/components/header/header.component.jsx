e534f481c450f3548d56e42dd55ea8a83b96d22f
Red-Fool48 <anirudhkumar2001@gmail.com>
2020-12-17 14:50:10 +0530

    a

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ const header=()=>
 {
     return(
         <div className='header'>
-            <Link className='logo-container' to="/Ecommwebsite"><Logo className='logo'/></Link>
+            <Link className='logo-container' to="/"><Logo className='logo'/></Link>
             <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT US</Link>
